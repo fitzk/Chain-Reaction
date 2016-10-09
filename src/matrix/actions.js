56e3fa2d3ce1731c85159aebab387fc3f071575f
fitzsimk <kfitzsimmons11@gmail.com>
2016-10-08 21:59:25 -0700

    add basic game behavior

--- src/matrix/actions.js
+++ src/matrix/actions.js
@@ -1,38 +1,17 @@
-import {Record} from 'immutable';
 import 'babel-polyfill';
 export const ADD_CELL = 'ADD_CELL';
-export const GET_CUBE = 'GET_CUBE';
 export const CRITICAL_MASS = 'CRITICAL_MASS';
 export const CLICK_CELL = 'CLICK_CELL';
-export const UNCLICK_CELL = 'UNCLICK_CELL';
 export const CLEAR_CELL = 'CLEAR_CELL';
 export const RESET_BOARD = 'RESET_BOARD';
-export const UPDATE_CELL = 'UPDATE_CELL';
-export const UPDATE_CELLS = 'UPDATE_CELLS';
-export const SET_ADJACENT_CELLS = 'SET_ADJACENT_CELLS';
-
 export const SET_PLAYER = 'SET_PLAYER';
 
-
-
-const Cube = new Record({
-  index: undefined,
-  cell_index: undefined,
-  color: undefined,
-});
-
-export function clickCell(clickedCell) {
+export function _clickCell(clickedCell) {
   return {
     type: CLICK_CELL, clickedCell
   };
 }
-
-export function unclickCell() {
-  let clickedCell = -1;
-  return {
-    type: CLICK_CELL, clickedCell
-  };
-}
+export const _badMove =()=>({type:'BAD_MOVE'});
 
 export function _addCell(cells, index, cell) {
   return {
@@ -46,11 +25,12 @@ export function _updateCriticalMass(criticalMass) {
   }
 }
 
-export function clearCell(cells,index) {
+export function clearCell(cells, index, cell) {
   return {
     type: CLEAR_CELL,
     cells,
-    index
+    index,
+    cell
   }
 }
 
@@ -60,13 +40,6 @@ export function resetBoard(cells) {
   }
 }
 
-export function setAdjacentCells(adjacentCells) {
-  return {
-    type: SET_ADJACENT_CELLS, adjacentCells,
-  }
-}
-
-
 const formatCells = (getState) => {
   if (!Array.isArray(getState().cells)) {
     return Object.keys(getState().cells).map(key => getState().cells[key]);
@@ -74,28 +47,47 @@ const formatCells = (getState) => {
   return getState().cells;
 };
 
-export const _setPlayer = (player) => ({ type: SET_PLAYER, player });
+export const _setPlayer = (player) => ({type: SET_PLAYER, player});
 
 export const play = () => (dispatch, getState) => {
-  let current_player = getState().players.current_player;
-  dispatch(addCubeToCell(current_player));
-  dispatch(endRound());
+  if(getState().clickedCell !== null) {
+    dispatch(addCubeToCell());
+    dispatch(endRound());
+  }
+};
 
+export const clickCell = (index) => (dispatch, getState) =>{
+  let currentPlayer = getState().players.current_player;
+  let owner = getState().cells[index].owner;
+  if(currentPlayer === owner) {
+    dispatch(_clickCell(index));
+  } else if (owner === null) {
+    dispatch(_clickCell(index));
+  } else {
+    dispatch(_clickCell(null));
+  }
 };
 
 export const getPlayerColor = (player) => {
-  switch(player){
-    case 0: return 'green';
-    case 1: return  'yellow';
-    default: return 'pink';
+  switch (player) {
+    case 0:
+    return 'green';
+    case 1:
+      return 'crimson';
+    case 'green':
+      return 0;
+    case 'crimson':
+      return 1;
+    default:
+      return 'green';
   }
 };
 
 export const endRound = () => (dispatch, getState) => {
   let current_player = getState().players.current_player;
   let next_player = 0;
-  if(current_player === 0) {
-    next_player=1;
+  if (current_player === 0) {
+    next_player = 1;
   }
   dispatch(_setPlayer(next_player));
 };
@@ -103,81 +95,86 @@ export const endRound = () => (dispatch, getState) => {
 export const addCubeToCell = () => (dispatch, getState) => {
   let player = getState().players.current_player;
   let cells = formatCells(getState);
+
   const color = getPlayerColor(player);
-  const cell_index = getState().clickedCell.index;
-  const old_cell = cells[cell_index];
-  const cube = makeCube(old_cell, cell_index, color);
-  let updated_cell = Object.assign({}, old_cell,
-    { critical_mass: old_cell.critical_mass,
-      mass: [...old_cell.mass, cube]
-    });
-  dispatch(_addCell(cells, cell_index, updated_cell));
-  dispatch(checkCriticalMass(cell_index));
-  dispatch(resolveCriticalMass());
+  const clicked_cell_index = getState().clickedCell.index;
+
+
+    const old_cell = cells[clicked_cell_index];
+    const new_cube = makeCube(old_cell, old_cell.mass.length, clicked_cell_index, color);
+
+    let new_cell = makeCell(old_cell.critical_mass, [...old_cell.mass, new_cube], player);
+
+    dispatch(_addCell(cells, clicked_cell_index, new_cell));
+    dispatch(checkCriticalMass(clicked_cell_index));
+    dispatch(resolveCriticalMass());
+
+
 };
 
 const resolveCriticalMass = () => (dispatch, getState) => {
   let critical_mass = getState().criticalMass;
-  console.log('critical mass:', critical_mass);
-  if(critical_mass.length > 0) {
-      let index = critical_mass.pop();
-
-      dispatch(clearCell(getState().cells, index));
-      let neighbors = getNeighbors(index);
-      console.log(neighbors);
-      for (let neighbor of neighbors) {
-        console.log(neighbor);
-        dispatch(clickCell(neighbor));
-        dispatch(addCubeToCell());
-      }
+  if (critical_mass.length > 0) {
+    let index = critical_mass.pop();
+    let old_cell = getState().cells[index];
+    let clear_cell=makeCell(old_cell.critical_mass,[],null);
+    dispatch(clearCell(getState().cells, index, clear_cell));
+    let neighbors = getNeighbors(index);
+    for (let neighbor of neighbors) {
+      dispatch(attemptTakeOver(neighbor));
+      dispatch(_clickCell(neighbor));
+      dispatch(addCubeToCell());
     }
+  }
   _updateCriticalMass([]);
 };
 
 const checkCriticalMass = (index) => (dispatch, getState) => {
   // check cell
-  console.log(getState().cells[index]);
-  if(getState().cells[index].mass.length === getState().cells[index].critical_mass){
+  if (getState().cells[index].mass.length === getState().cells[index].critical_mass) {
     const currentMass = getState().criticalMass;
-    let criticalMass = Object.assign([],currentMass,[...currentMass,index]);
+    let criticalMass = Object.assign([], currentMass, [...currentMass, index]);
     dispatch(_updateCriticalMass(criticalMass));
   }
 };
-
-const _updateCell = (cell) => ({ type: UPDATE_CELL });
-const _updateCells = (cells) => ({ type: UPDATE_CELLS, cells });
-
-const updateCell = (cell) => (dispatch, getState) => {
-  const cells = formatCells(getState);
-  const index = cells.indexOf(cell);
-  const updatedCell = Object.assign({}, cells[index], cell);
-  dispatch(_updateCell(index));
-  const updatedCells = Object.assign([], cells, updatedCell);
-  dispatch(_updateCells(updatedCells));
+export const makeCell = (critical_mass, mass, owner) => {
+  return {
+    critical_mass,
+    mass,
+    owner
+  };
 };
-
-export function getNeighbors(cellIndex) {
-  return [cellIndex-1,cellIndex - 10, cellIndex + 1, cellIndex + 10];
-}
-
-export const makeCube = (cell, index, color) => {
+export const makeCube = (cell, cube_index, cell_index, color) => {
   if (cell) {
     return {
-      index: cell.mass.length,
-      cell_index: index,
+      index: cube_index,
+      cell_index: cell_index,
       color: color,
     };
 
   }
 };
 
-export const makeCell = (critical_mass, mass) => {
-    return {
-      critical_mass,
-      mass
-    };
+export const attemptTakeOver = (index) => (dispatch, getState) => {
+  let neighbor_cell = getState().cells[index];
+  let current_player = getState().players.current_player;
+  if (neighbor_cell.mass.length > 0 && neighbor_cell.owner !== current_player){
+    let new_mass = [];
+    for(let cube of neighbor_cell.mass) {
+      let new_cube = makeCube(neighbor_cell, cube.index, cube.cell_index, getPlayerColor(current_player));
+      new_mass.push(new_cube);
+    }
+    let old_cell = getState().cells[index];
+    let clear_cell=makeCell(old_cell.critical_mass,[],null);
+    dispatch(clearCell(getState().cells, index, clear_cell));
+    let cell = makeCell(neighbor_cell.critical_mass, new_mass, current_player);
+    dispatch(_addCell(getState().cells, index, cell));
+  }
 };
 
+export function getNeighbors(cellIndex) {
+  return [cellIndex - 1, cellIndex - 10, cellIndex + 1, cellIndex + 10];
+}
 
 export const generateBoard = () => {
   return (dispatch, getState) => {
@@ -191,7 +188,7 @@ export const generateBoard = () => {
     const cells = indexes.map(index => {
       for (var idx of [...corners]) {
         if (index == idx) {
-          return {critical_mass: 2, mass: []};
+          return makeCell(2,[],null);
         }
       }
       for (idx of [...top_row,
@@ -199,10 +196,10 @@ export const generateBoard = () => {
         ...right_col,
         ...bottom_row]) {
         if (index == idx) {
-          return {critical_mass: 3, mass: []};
+          return makeCell(3,[],null);
         }
       }
-      return {critical_mass: 4, mass: []};
+      return makeCell(4,[],null);
     });
 
     dispatch(resetBoard(cells));
--- src/matrix/reducers.js
+++ src/matrix/reducers.js
@@ -1,38 +1,24 @@
-import { combineReducers } from 'redux';
+import {combineReducers} from 'redux';
 import {
   RESET_BOARD,
   ADD_CELL,
   CLEAR_CELL,
-  SET_ADJACENT_CELLS,
   CLICK_CELL,
-  UNCLICK_CELL,
-  UPDATE_CELLS,
   CRITICAL_MASS,
-  UPDATE_CELL,
   SET_PLAYER,
 } from './actions.js';
 
-export const adjacentCells = (state={}, action) => {
-  switch(action.type) {
-    case SET_ADJACENT_CELLS:
-      console.log(action);
-      return Object.assign({}, state, action.adjacentCells );
+export const clickedCell = (state = {}, action) => {
+  switch (action.type) {
+    case CLICK_CELL:
+      let clickedCell = action.clickedCell;
+      return Object.assign({}, state, {index: clickedCell});
     default:
       return state;
   }
 };
 
-export const clickedCell = (state={}, action) => {
-  switch (action.type) {
-    case CLICK_CELL || UNCLICK_CELL:
-      let clickedCell = action.clickedCell;
-      return Object.assign({}, state, {index: clickedCell});
-  default:
-  return state;
-  }
-};
-
-export const criticalMass = (state=[], action) => {
+export const criticalMass = (state = [], action) => {
   switch (action.type) {
     case CRITICAL_MASS:
       return Object.assign([], state, action.criticalMass);
@@ -41,45 +27,29 @@ export const criticalMass = (state=[], action) => {
   }
 };
 
-export const players = ( state={ current_player:0, total_players:2 }, action) => {
-  switch(action.type) {
+export const players = (state = {current_player: 0, total_players: 2}, action) => {
+  switch (action.type) {
     case SET_PLAYER:
-      return Object.assign({}, state, {current_player:action.player});
+      return Object.assign({}, state, {current_player: action.player});
     default:
       return state;
   }
 };
 
-export const cells = (state=[], action) => {
-    switch(action.type) {
-      case UPDATE_CELLS:
-        return Object.assign([], state, action.cells);
-      case ADD_CELL:
-        return Object.assign([], state, action.cells[action.index] = action.cell);
-      case CLEAR_CELL:
-        console.log(action);
-        return Object.assign([], state,
-          action.cells.map((cell, index)=> {
-            if (index === action.index) {
-              console.log(cell, index);
-              return Object.assign({}, cell, {
-                critical_mass: action.cells[action.index].critical_mass,
-                mass: []
-              });
-            } else {
-              return Object.assign({}, cell, action.cells[action.cells.indexOf(cell)]);
-            }
-          }),
-        );
-      case RESET_BOARD:
-            return Object.assign([], state,  [...action.cells]);
-      default:
-            return state;
-    }
+export const cells = (state = [], action) => {
+  switch (action.type) {
+    case UPDATE_CELLS:
+      return Object.assign([], state, action.cells);
+    case ADD_CELL:
+      return Object.assign([], state, action.cells[action.index] = action.cell);
+    case CLEAR_CELL:
+      return Object.assign([], state, action.cells[action.index] = action.cell);
+    case RESET_BOARD:
+      return Object.assign([], state, [...action.cells]);
+    default:
+      return state;
+  }
 };
-
-
-
 
 const rootReducer = combineReducers({
   cells,
